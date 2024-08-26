b636d5370a46d42a166b8ec19c819ea56ea0755e
Jamerson <jamescao800@gmail.com>
2024-08-25 21:17:57 -0400

    added the featurets

--- checkmint_website/src/app/page.tsx
+++ checkmint_website/src/app/page.tsx
@@ -5,6 +5,8 @@ import { MacbookScroll } from "../components/macbook";
 import { Compare } from "../components/compare";
 import { StickyScroll } from "../components/scroll-reveal";
 import React from "react";
+import { HoverEffect } from "../components/card-hover";
+
 import {
   Modal,
   ModalBody,
@@ -172,9 +174,12 @@ export default function Home() {
           initialSliderPercentage={25}
         />
       </div>
-      <div className="overflow-y-scroll bg-[#F1F6F9] mt-11 no-scrollbar">
-        <h1 className="text-4xl">Features</h1>
+      <div className="overflow-y-scroll bg-[#F1F6F9] mt-11 no-scrollbar ">
+        <h1 className="text-4xl">How to use it</h1>
         <StickyScroll content={content} />
+        <div className="max-w-5xl px-8 mx-auto">
+          <HoverEffect items={projects} />
+        </div>
       </div>
     </main>
   );
@@ -309,3 +314,42 @@ const PlaneIcon = ({ className }: { className?: string }) => {
     </svg>
   );
 };
+
+export const projects = [
+  {
+    title: "Stripe",
+    description:
+      "A technology company that builds economic infrastructure for the internet.",
+    link: "https://stripe.com",
+  },
+  {
+    title: "Netflix",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+    link: "https://netflix.com",
+  },
+  {
+    title: "Google",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "https://google.com",
+  },
+  {
+    title: "Meta",
+    description:
+      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+    link: "https://meta.com",
+  },
+  {
+    title: "Amazon",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
+  {
+    title: "Microsoft",
+    description:
+      "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+    link: "https://microsoft.com",
+  },
+];
